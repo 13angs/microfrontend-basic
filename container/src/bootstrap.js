777b89de98362dc014b550d74d136004edab93e1
13angs <13angz.um@gmail.com>
2022-08-23 02:19:01 +0700

    local deploy mfb using docker

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -2,17 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const mount = (element) => {
-    const root = createRoot(element);
-    root.render(<App/>);
-}
-
-if(process.env.NODE_ENV === 'development'){
-    const devRoot = document.getElementById('container-id')
-
-    if(devRoot){
-        mount(devRoot);
-    }
-}
-
-export {mount}
+const container = document.getElementById('container-id');
+const root = createRoot(container);
+root.render(<App/>)
