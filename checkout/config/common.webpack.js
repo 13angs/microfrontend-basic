a108f330f3503a29853672a39e6381ad7bd03a81
13angs <13angz.um@gmail.com>
2022-08-22 10:59:52 +0000

    add css support

--- checkout/config/common.webpack.js
+++ checkout/config/common.webpack.js
@@ -13,6 +13,14 @@ module.exports = {
                         plugins: ['@babel/plugin-transform-runtime']
                     }
                 }
+            },
+
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
             }
         ]
     },
