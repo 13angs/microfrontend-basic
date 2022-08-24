f19758e021945211f70b3c040e6eaf64df5d3ed3
suha3001 <suhabaka01@gmail.com>
2022-08-24 15:00:18 +0000

    props open dialog

--- home/src/components/ContainerPage.js
+++ home/src/components/ContainerPage.js
@@ -1,28 +1,32 @@
 import React from "react";
-import {containerMount} from 'container/Components';
+import { containerMount } from 'container/Components';
 
-function ContainerApp({value}){
+function ContainerApp({ value, setValue }) {
     const appRef = React.useRef(null);
-// console.log(containerMount)
-React.useEffect(() => {
-    if(appRef.current && containerMount)
-    {
-        containerMount(appRef.current, value);
-    }
-})
-return <div ref={appRef}/>
+    // console.log(containerMount)
+    React.useEffect(() => {
+        if (appRef.current && containerMount) {
+            containerMount(appRef.current, value, setValue);
+        }
+    })
+    return <div ref={appRef} />
 }
 
-export default function ContainerPage(){ 
+export default function ContainerPage() {
     const [open, setOpen] = React.useState(false);
+
+    console.log(open)
     const handleClick = () => {
         setOpen(true)
     }
-    
+    const handleClose = () => {
+        setOpen(false)
+    }
+
     return (
         <div>
             <button onClick={handleClick}>OPEN DIALOG</button>
-            <ContainerApp value={open} />
+            <ContainerApp value={open} closeDialog={setOpen} />
         </div>
     );
 }
