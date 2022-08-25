27cc9be26f089e410f42dbea0b7f9695a72c9123
suha3001 <suhabaka01@gmail.com>
2022-08-25 04:48:46 +0000

    convert cra to webpack

---
+++ profile/config/dev.webpack.js
@@ -0,0 +1,37 @@
+const {merge} = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./common.webpack');
+// const packageJson = require('../package.json');
+const path = require('path');
+const webpack = require('webpack');
+
+const devConfig = {
+    mode: 'development',
+    output: {
+        publicPath: 'http://localhost:3004/',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 3004,
+        host: 'localhost',
+        historyApiFallback: true
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+            name: 'profile',
+            filename: 'remoteEntry.js',
+            // exposes: {
+
+            // }
+        }),
+
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+
+        new webpack.EnvironmentPlugin({})
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
